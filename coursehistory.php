f2e06704780242d07c02a71c501dfc6168018127
MatthewHunt <matthew.hunt01@gmail.com>
2015-04-23 18:31:36 -0500

    Create coursehistory.php

---
+++ coursehistory.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html><head>
+<?php
+//error_reporting(0);
+session_start();  
+?>
+        <meta charset="UTF-8" />
+    
+        <title>Homepage</title>
+        <head>
+        <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="styles.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+    
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>index</title>
+    
+    <script src="js/vendor/modernizr.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="Login and Registration Form with HTML5 and CSS3" />
+        <meta name="keywords" content="html5, css3, form, switch, animation, :target, pseudo-class" />
+        <meta name="author" content="Codrops" />
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" type="text/css" href="css/animate-custom.css" />
+    </head>
+<body>
+        <div class="container">
+         
+            <div class="codrops-top">
+                <a href="">	Authors: Brandon, Chaz and Matthew. </a>
+                <span class="right">
+                    
+                        <strong>Senior Software and Professional Practice</strong>
+                    </a>
+                </span>
+              <div class="clr"></div>
+    </div>
+            <header>
+                <h1>Welcome to <span>Computer Science Advisor</span></h1>
+				<nav class="codrops-demos">	
+                <div id='cssmenu'>
+<ul>
+   <li><a href='advisor.php'><span>Advisor</span></a></li>
+   <li><a href='coursehistory.php'><span>View/Edit Course History</span></a></li>
+   <li><a href='usersettings.php'><span>Settings</span></a></li>
+   <li><a href='logout.php'><span>Log Out</span></a></li>
+  
+  
+  
+   
+   
+</ul>
+</div>
+				  
+				  
+				</nav>
+            </header>
+            <section>				
+                <div id="container_demo" >
+                    
+                    <a class="hiddenanchor" id="toregister"></a>
+                    <a class="hiddenanchor" id="tologin"></a>
+                    <div id="wrapper">
+    <h5>Course History</h5>
+ 
+		<p>&nbsp;</p>
+		
+       <form  action= "index.html" method="post"> 
+				  <div class="row">
+			      <div class="large-12 columns"></div>
+    </div>
+			    <div class="row">
+			      <div class="large-4 medium-4 columns">
+				      <div class="row collapse">
+<div class="small-3 columns"></div>
+					  </div>
+				  </div>
+				</div><!--row-->
+				
+				  <div class="row">
+<div class="large-12 columns"></div>
+				  </div>
+				  <div class="row"> </div>
+       <p>&nbsp;  </p> 
+     
+     <!-- LOOK HERE MATT! -->
+     
+      <p>Work in progress</p> 
+      
+      
+      
+      
+      
+      
+
+</body>
+
+</html>
